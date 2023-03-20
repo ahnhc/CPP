3b6f1652aa751b06685c455cf0db0901508f4dfb
Hyeongchan <128343208+ahnhc@users.noreply.github.com>
2023-03-21 03:09:42 +0900

    Update aim.py

--- aim.py
+++ aim.py
@@ -4,4 +4,13 @@ print("Hello AIM!")
 
 # 코딩 문제 2번
 
-print()
+a = 4
+star = '*'
+blank = ' '
+star_start = 1
+
+for i in range(a,0,-1):
+    print(blank * (i-1) + star_start * star)
+    star_start +=2
+    if star_start == 5:
+        print(blank * (i-2),"AIM")
