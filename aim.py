2f99fcea39be8d095eb2f74f4dc02abf4481956f
Hyeongchan <128343208+ahnhc@users.noreply.github.com>
2023-03-21 03:45:34 +0900

    수정중.. 3번 모르겠음

--- aim.py
+++ aim.py
@@ -1,20 +1,32 @@
 # 코딩 문제 1번
 
-print("Hello AIM!")
+# print("Hello AIM!")
 
 # 코딩 문제 2번
 
-a = 4
-star = '*'
-blank = ' '
-star_start = 1
-
-for i in range(a,0,-1):
-    print(blank * (i-1) + star_start * star)
-    star_start +=2
-    if star_start == 5:
-        print(blank * (i-2),"AIM")
+# a = 4
+# star = '*'
+# blank = ' '
+# star_start = 1
+#
+# for i in range(a,0,-1):
+#     print(blank * (i-1) + star_start * star)
+#     star_start +=2
+#     if star_start == 5:
+#         print(blank * (i-2),"AIM")
 
 # 코딩 문제 3번
-
-
+# no_num = 0
+# major = []
+# class_of = []
+# mbti = []
+# name = []
+# answer = 'Yes'
+# while answer != 'No':
+#     str(input(major.append))
+#     input(class_of.append)
+#     str(input(mbti.append))
+#     str(input(name.append))
+#     no_num+=1
+#     print("{}. {}.ljust(8) {}.ljust(2)학번 {}.ljust(4) {}.ljust(6)".format(no_num(no_num),major(no_num),class_of(int(no_num)),mbti(no_num),name(no_num)))
+#     answer = str(input(print("더 입력하시겠습니까? (Yes/No)")))
