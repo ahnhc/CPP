8b454361a1cb6f1165740634351289335904f456
Hyeongchan Ahn <128343208+ahnhc@users.noreply.github.com>
2023-03-22 16:25:46 +0900

    Update aim.py

--- aim.py
+++ aim.py
@@ -1,19 +1,19 @@
 # 코딩 문제 1번
 
-# print("Hello AIM!")
+print("Hello AIM!")
 
 # 코딩 문제 2번
 
-# a = 4
-# star = '*'
-# blank = ' '
-# star_start = 1
-#
-# for i in range(a,0,-1):
-#     print(blank * (i-1) + star_start * star)
-#     star_start +=2
-#     if star_start == 5:
-#         print(blank * (i-2),"AIM")
+a = 4
+star = '*'
+blank = ' '
+star_start = 1
+
+for i in range(a,0,-1):
+    print(blank * (i-1) + star_start * star)
+    star_start +=2
+    if star_start == 5:
+        print(blank * (i-2),"AIM")
 
 # 코딩 문제 3번
 # no_num = 0
