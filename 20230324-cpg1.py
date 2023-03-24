ae7e3161e4c73e9551079ad43c66a03ae155272f
Hyeongchan Ahn <128343208+ahnhc@users.noreply.github.com>
2023-03-24 14:48:36 +0900

    Add files via upload

--- 20230324-cpg1.py
+++ 20230324-cpg1.py
@@ -100,3 +100,46 @@
 
 # infinite loop and break & continue
 
+# for i in range(1, 11):
+#     print(i, end="")
+#     if i == 7:
+#         break
+
+# print("### Who's in front of me ###")
+# name = input("Enter the name : ")
+# wait_list = ['Emma','Angela','Helena','Nicole','Elizabeth','Tina']
+# for i in wait_list:
+#     print(i, end=" ")
+#     if i == name:
+#         break
+
+# exercise : color of traffic light
+
+# while True:
+#     light = input('Enter the color of traffic light (red/yellow/green): ')
+#     if light == 'green':
+#         break
+# print( 'Go!!')
+
+# exercise : times table test
+
+num_list = []
+answer = 'y'
+while answer == 'y':
+    nums = int(input("Which times table do you test? (Enter nums btw 2 and 9) : "))
+    num_list.append(nums)
+    answer = str(input("More?(y/n) : "))
+print(f"We will test {num_list} times table.")
+
+exam_list = []
+while i in range(num_list):
+    exam = random.randint(1, 10)
+    exam_list.append(exam)
+    i+=1
+
+problems = 3*range(num_list)
+print(f"We will test {problems} problems.")
+
+while j in range(num_list):
+    print(f"{num_list[j]} * {exam_list[j]} = {}")
+    j+=1
