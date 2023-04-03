dc4696565c998ce16bb576dda54469ee1aeda5b4
Hyeongchan Ahn <128343208+ahnhc@users.noreply.github.com>
2023-04-03 13:40:16 +0900

    Add files via upload

---
+++ 20230403-cpg1.py
@@ -0,0 +1,52 @@
+# list?
+# to use data efficiently
+
+# temps =[1,2,3,4,5,6]
+# for i in temps:
+#     print(i,end=" ")
+#
+# for i in range(len(temps)):
+#     print(temps[i],end=" ")
+
+# fruits=[]
+# print(fruits)
+# fruits.append("apple")
+# print(fruits)
+# fruits.append("banana")
+# print(fruits)
+
+# fruits = ["apple","banana","grape"]
+# fruits.insert(1,"cherry")
+# print(fruits)
+
+# sort()
+
+# temps = [28,31,33,35,27,26,25]
+
+# temps.sort()
+# print(temps)
+# temps.sort(reverse=True)
+# print(temps)
+
+# a = sorted(temps)
+# print(a)
+
+# 기존의 함수를 뒤집어서 출력해주는 함수 : reversed
+# d = reversed(temps)
+# print(list(d))
+
+# import random
+# numberlist = [1,2,3,4,5,6,7,8,9,10]
+# print(random.choice(numberlist))
+# movielist = ["c","s","m","ca","ci"]
+# item = random.choice(movielist)
+# print(item)
+
+# shallow copy of list
+# temps = [28,31,33,35,27,26,25]
+# values = temps
+# print("values: ",values)
+# print("temps: ",temps)
+# values[3] = 39
+# print("values: ",values)
+# print("values: ",values)
